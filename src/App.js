6b3c40fedd946774865c3ac26854bb547c72d99d
Ramiro <krupoviesaramiro@gmail.com>
2020-10-10 09:53:40 -0300

    with-frontend

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static("build"));
 
 app.use("/products", productsRouters);
 app.use("/users", usersRouters);
