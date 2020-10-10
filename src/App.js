3a5153ef8cbf3d5f31c05e16199b72e72b8fe632
Ramiro <krupoviesaramiro@gmail.com>
2020-10-10 11:27:16 -0300

    fixed-routes

--- src/App.js
+++ src/App.js
@@ -6,15 +6,23 @@ const productsRouters = require("./routers/products.router");
 const usersRouters = require("./routers/user.router");
 require("./db/mongoose");
 
+const path = require("path");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(express.static("build"));
 
 app.use("/products", productsRouters);
 app.use("/users", usersRouters);
 app.use("/cart", cartRouters);
+
+/* catch-all route to index.html */
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve("build/index.html"));
+});
 
 module.exports = app;
