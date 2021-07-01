0d135bc35ae2744d011d7da670667d97f14bd61a
Luis Ramiro Krupoviesa <ramiro@pop-os.localdomain>
2021-07-01 15:28:37 -0300

    updated fronted build

---
+++ build/precache-manifest.519a399df1207cb5c8992aba9acdbcdc.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c009cbe76c6871bb701649a0417a791",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef2d0795b1db531f7baf",
+    "url": "/static/css/main.c060ac2c.chunk.css"
+  },
+  {
+    "revision": "6a6e17238e67218c415d",
+    "url": "/static/js/2.bed0bb01.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bed0bb01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef2d0795b1db531f7baf",
+    "url": "/static/js/main.a13cc615.chunk.js"
+  },
+  {
+    "revision": "252f7176bc47fad329f8",
+    "url": "/static/js/runtime-main.bb340dc9.js"
+  },
+  {
+    "revision": "0f3dedb4599b5d198439d5d0dd1e64f3",
+    "url": "/static/media/bell.0f3dedb4.svg"
+  },
+  {
+    "revision": "b2f89f1d8b1c0b78fa60fa8ceec21079",
+    "url": "/static/media/canned.b2f89f1d.svg"
+  },
+  {
+    "revision": "bfa2167949226e562410367a05e34b91",
+    "url": "/static/media/closed.bfa21679.svg"
+  },
+  {
+    "revision": "e355ca0675c3c4468f4dbe584b384eb3",
+    "url": "/static/media/curveAsymmetrical.e355ca06.svg"
+  },
+  {
+    "revision": "ba49eba7ca12915a48dcc027e6750469",
+    "url": "/static/media/curveAsymmetrical2.ba49eba7.svg"
+  },
+  {
+    "revision": "90d35cab9b4b8251bd2007f93e3b6840",
+    "url": "/static/media/fruits.90d35cab.svg"
+  },
+  {
+    "revision": "6181efb3bf841cee1591c1110973e8c6",
+    "url": "/static/media/history.6181efb3.svg"
+  },
+  {
+    "revision": "04414ae260c09767f23b22c4f5c2c85f",
+    "url": "/static/media/logo.04414ae2.svg"
+  },
+  {
+    "revision": "985bc595fcd7000d3438048df9948c32",
+    "url": "/static/media/logout.985bc595.svg"
+  },
+  {
+    "revision": "bef92af7cae08d30ca16e598e4a6d709",
+    "url": "/static/media/magnifying-simple.bef92af7.svg"
+  },
+  {
+    "revision": "3ca5a974cc9281c7cd06f6e5a834d781",
+    "url": "/static/media/magnifying.3ca5a974.svg"
+  },
+  {
+    "revision": "ac42d435b597d67e8527ec6747b9ef59",
+    "url": "/static/media/medal.ac42d435.svg"
+  },
+  {
+    "revision": "512f17f207b3c63345cefe0f7eb8f0ff",
+    "url": "/static/media/more-products.512f17f2.svg"
+  },
+  {
+    "revision": "81d0b26d5cadbf39f74251b3661a54f3",
+    "url": "/static/media/new.81d0b26d.svg"
+  },
+  {
+    "revision": "431bcbdef394046e826d20d763383194",
+    "url": "/static/media/orders.431bcbde.svg"
+  },
+  {
+    "revision": "5be5fc5aec0c54c3b31b08f99654fbfd",
+    "url": "/static/media/security.5be5fc5a.svg"
+  },
+  {
+    "revision": "4096aeb6dfad61c80c38a2d7d4eed6bc",
+    "url": "/static/media/shopping-cart.4096aeb6.svg"
+  },
+  {
+    "revision": "65e84431175c50c96d6277f33de327b8",
+    "url": "/static/media/snacks.65e84431.svg"
+  },
+  {
+    "revision": "4c770f12f4a4eab37c2bbd488f6e73f3",
+    "url": "/static/media/spices.4c770f12.svg"
+  },
+  {
+    "revision": "e42fd0e4eb98069711a866a4c6cef579",
+    "url": "/static/media/spinner.e42fd0e4.svg"
+  },
+  {
+    "revision": "75aed98cb52722e75858f4dece061021",
+    "url": "/static/media/vegetables.75aed98c.svg"
+  }
+]);
